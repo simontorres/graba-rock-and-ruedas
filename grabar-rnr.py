6287d3a1c9a7e6053d734a64e36045c27edb9522
Simon Torres <storres@ctio.noao.edu>
2018-05-25 14:17:40 -0400

    updated timer to wait until next emission

--- grabar-rnr.py
+++ grabar-rnr.py
@@ -47,34 +47,15 @@ class StreamRecorder(object):
         self.duration = int(duration.total_seconds())
 
         while True:
-            if not self.__check_weekday():
-                # self.__wait_until_monday()
-                sys.exit('No hay Rock & Ruedas hoy {:s}.'.format(self.day_name))
-            if self.__get_time_to_start() < 0:
-                print('A esperar hasta manana')
-                time.sleep(3600)
 
-            elif self.__get_time_to_start() > 3600:
-                print("Waiting 1 Hour")
-                print("Next Check: {:s}".format(str(datetime.datetime.now() +
-                                                datetime.timedelta(hours=1))))
-                time.sleep(3600)
-
-            elif self.__get_time_to_start() > 600:
-                print("Waiting 10 Minutes")
-                print("Next Check: {:s}".format(str(datetime.datetime.now() +
-                                                    datetime.timedelta(
-                                                        minutes=10))))
-                time.sleep(600)
+            if self.__get_time_to_start() < - self.duration:
+                if True:
+                    till_monday = datetime.timedelta(
+                        seconds=self.__get_time_to_start()) + datetime.timedelta(days=(7 - self.today.weekday()))
+                    self.__wait(seconds=till_monday.total_seconds())
             else:
                 while self.__get_time_to_start() > 1:
-                    print("\rTime to start {:s}".format(
-                        str(datetime.timedelta(
-                            seconds=self.__get_time_to_start()))),
-                        sep=' ',
-                        end='',
-                        flush=True)
-                    time.sleep(1)
+                    self.__wait(seconds=self.__get_time_to_start())
 
                 print("Start recording")
                 self.output_file = self.output_base.format(str(self.today),
@@ -90,9 +71,6 @@ class StreamRecorder(object):
                 self.command = self.command_base.format(self.output_file,
                                                         self.stream_source)
                 self.__record()
-
-
-            break
 
     def __record(self):
         try:
@@ -131,12 +109,20 @@ class StreamRecorder(object):
 
         start_time = pytz.timezone('America/Santiago').localize(time_obj)
         time_to_start = start_time - now
-        print(time_to_start.total_seconds())
         return time_to_start.total_seconds()
 
     def __stop_recording(self, process):
         print("End recording")
         process.kill()
+
+    def __wait(self, seconds):
+        print("\rTime to start {:s}".format(
+            str(datetime.timedelta(
+                seconds=int(seconds)))),
+            sep=' ',
+            end='',
+            flush=True)
+        time.sleep(1)
 
 if __name__ == '__main__':
     record = StreamRecorder()
@@ -144,11 +130,3 @@ if __name__ == '__main__':
         record()
     except KeyboardInterrupt:
         sys.exit("Program Exit")
-
-#
-# for line in stdout.split(b'\n'):
-#     print(line.decode("utf-8"))
-#
-# if stderr != b'':
-#     for error_line in stderr.split(b'\n'):
-#         print("error: ", error_line.decode("utf-8"))
